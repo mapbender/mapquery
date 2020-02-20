9e9cc81a5ea847e8984eb8960a5b860d7685195e
werrolf <rolf.neuberger@wheregroup.com>
2020-02-20 13:55:37 +0100

    Provide OpenLayers.js.map

--- lib/update-openlayers.sh
+++ lib/update-openlayers.sh
@@ -10,7 +10,9 @@ mkdir ol2-dist
   ol2-dist/readme.md \
   openlayers/ ) \
 && ( cat ol2-dist/build/license.txt patch-info.js > OpenLayers.debug.min.js.build ) \
-&& ( cat openlayers/OpenLayers.debug.js | uglifyjs -c -m  >> OpenLayers.debug.min.js.build ) \
+&& ( uglifyjs openlayers/OpenLayers.debug.js -c -m \
+   -p 1 --source-map=openlayers/OpenLayers.js.map --source-map-url=OpenLayers.js.map \
+   >> OpenLayers.debug.min.js.build ) \
 && ( mv OpenLayers.debug.min.js.build openlayers/OpenLayers.js )
 
 rm -rf ol2-dist
